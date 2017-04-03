977c6207f13d1371ae69c71d4c36a0750767d5e5
u5564556 <u5564556@anu.edu.au>
2017-04-03 16:54:08 +1000

    "Changed colour of mainactivity to be the same as Prototype. Attempted to remove action bar (top bar) but coudl only make it the same colour as background (has a line...).

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/MainActivity.java
@@ -6,16 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.*;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        if (getActionBar() != null){
+            getActionBar().hide();
+        }
     }
 
 }
