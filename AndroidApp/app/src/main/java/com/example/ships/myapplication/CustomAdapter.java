05f44efb50973c05cfc96da88b9b199f29ac7d50
Yijuan Liang <u5855756@anu.edu.au>
2017-04-03 13:58:29 +1000

    added checkList adapter

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/CustomAdapter.java
@@ -0,0 +1,41 @@
+package com.example.ships.myapplication;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Jyun on 2017/04/03.
+ */
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter<Programs> {
+    ArrayList<Programs> programsItems = null;
+    Context context;
+    public CustomAdapter(Context context, ArrayList<Programs> resource) {
+        super(context,R.layout.row_programs,resource);
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.programsItems = resource;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        convertView = inflater.inflate(R.layout.row_programs, parent, false);
+        TextView name = (TextView) convertView.findViewById(R.id.ProgamTitle);
+        CheckBox cb = (CheckBox) convertView.findViewById(R.id.checkProgram);
+        name.setText(programsItems.get(position).getName());
+        if(programsItems.get(position).getValue() == 1)
+            cb.setChecked(true);
+        else
+            cb.setChecked(false);
+        return convertView;
+    }
+}
--- AndroidApp/app/src/main/java/com/example/ships/myapplication/FutureProgram.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/FutureProgram.java
@@ -8,13 +8,32 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class FutureProgram extends AppCompatActivity {
+    ListView lv;
+    ArrayList<Programs> programsItems = new ArrayList<Programs>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_future_program);
+        lv = (ListView) findViewById(R.id.programList);
 
+        //add item to the list
+        programsItems.add(new Programs("Program 1", 0));
+        programsItems.add(new Programs("Program 2", 1));
+        programsItems.add(new Programs("Program 3", 1));
+        programsItems.add(new Programs("Program 4", 0));
+        programsItems.add(new Programs("Program 5", 1));
+        CustomAdapter adapter = new CustomAdapter(this, programsItems);
+        lv.setAdapter(adapter);
+    }
+
+    public void addToMyProgram(View view) {
+        //get programs' name and all to user's list
     }
 }
