a450dc9858b8396f3ad3a4e51a336235ca9c8354
Yijuan Liang <u5855756@anu.edu.au>
2017-04-03 11:34:46 +1000

    initial Future Program page

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/FutureProgram.java
@@ -0,0 +1,20 @@
+package com.example.ships.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FutureProgram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
