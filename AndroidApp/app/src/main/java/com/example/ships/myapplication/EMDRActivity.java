e007811872ddb0b53cd25628d01110d437c447c9
Lalit Prasad <lalitpras@gmail.com>
2017-04-05 13:48:14 +1000

    achieved small one-time movement of circle on screen

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/EMDRActivity.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/EMDRActivity.java
@@ -1,11 +1,20 @@
 package com.example.ships.myapplication;
 
+import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static com.example.ships.myapplication.R.layout.animationemdr;
 
 public class EMDRActivity extends AppCompatActivity {
 
@@ -16,14 +25,29 @@ public class EMDRActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TranslateAnimation moveLeftToRight = new TranslateAnimation(0, 400, 0, 0);
+        moveLeftToRight.setDuration(1000);
+        moveLeftToRight.setFillAfter(true);
+
+        Animation emdrMovement;
+        LinearLayout layoutEMDRMovement = (LinearLayout) findViewById(R.id.emdrlayout);
+        layoutEMDRMovement.startAnimation(moveLeftToRight);
+
+
+//
+//        TextView testText = new TextView(this);
+//        testText.setText("Test");
+//        testText.startAnimation(moveLeftToRight);
+
+
+
+
+//        emdrMovement = AnimationUtils.loadAnimation(this, R.layout.animationemdr);
+//        emdrMovement.reset();
+//        emdrMovement.setFillAfter(true);
+//        emdrMovement.setAnimationListener((Animation.AnimationListener) this);
+//        layoutEMDRMovement.startAnimation(emdrMovement);
+
     }
 
 }
