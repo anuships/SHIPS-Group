2703c5315efaf6658c098816157cacd25e3d7df4
u5564556 <u5564556@anu.edu.au>
2017-04-27 13:54:26 +1000

    fixed some minor issues.

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/FAS/ExposureInfo.java
@@ -0,0 +1,16 @@
+package com.example.ships.myapplication.FAS;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ships.myapplication.R;
+
+public class ExposureInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_exposure_info);
+    }
+}
