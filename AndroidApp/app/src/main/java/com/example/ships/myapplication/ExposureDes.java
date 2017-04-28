ab9900b367c03d263c2c262086ee7e06b7e0a8d7
Ka Wai Ma <u5968223@anu.edu.au>
2017-04-28 23:01:08 +1000

    Add exposure

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/ExposureDes.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/ExposureDes.java
@@ -1,14 +1,20 @@
 package com.example.ships.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ExposureDes extends AppCompatActivity {
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exposure_des);
     }
+    public void startTherapy(View v){
+        startActivity(new Intent(this, ExposureTherapy.class));
+    }
+
 }
