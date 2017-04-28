2ff0ca2119f81fdb3fdf3c8eb06274cd8516ae89
Yijuan  Liang <u5855756@anu.edu.au>
2017-04-28 23:10:25 +1000

    protection test 3

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/ExposureDes.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/ExposureDes.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class ExposureDes extends AppCompatActivity {
-
+//Kevin's protection test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
