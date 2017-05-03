3b21778db606906495ea3b4c23024526f0cf0ed9
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 23:06:34 +1000

    created 'systematic desensitation' page

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/SystematicDesensitation.java
@@ -0,0 +1,19 @@
+package com.example.ships.myapplication.OtherInterfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ships.myapplication.R;
+
+public class SystematicDesensitation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_systematic_desensitation);
+    }
+
+    public void goBack(View view) {
+    }
+}
