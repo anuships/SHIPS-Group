648da8cfda625bea962d123f01faef130d9bbdcf
Yiuan Liang <u5855756@anu.edu.au>
2017-05-04 00:41:55 +1000

    created 'user profile' page

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/UserProfile.java
@@ -0,0 +1,31 @@
+package com.example.ships.myapplication.OtherInterfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ships.myapplication.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+
+    public void goToChangePassword(View view) {
+    }
+
+    public void goToUpdateUserInformation(View view) {
+    }
+
+    public void goToSuggestedModule(View view) {
+    }
+
+    public void goToMyProgram(View view) {
+    }
+
+    public void goToRecords(View view) {
+    }
+}
