65a14caca6907a7d30a39401bcdcdb54acf6d341
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 19:21:30 +1000

    changed button nanmes

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/ConfirmeChangePW.java
@@ -0,0 +1,15 @@
+package com.example.ships.myapplication.OtherInterfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ships.myapplication.R;
+
+public class ConfirmeChangePW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirme_change_pw);
+    }
+}
