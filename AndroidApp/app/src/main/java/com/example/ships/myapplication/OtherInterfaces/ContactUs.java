11f1b76b9c8461c44a03d986e6adf482f614b715
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 19:31:39 +1000

    added cancel function

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/ContactUs.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/ContactUs.java
@@ -2,19 +2,14 @@ package com.example.ships.myapplication.OtherInterfaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.ships.myapplication.R;
 
-public class ChangePassword extends AppCompatActivity {
+public class ContactUs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
-    }
-
-    public void cencel(View view) {
-        super.onBackPressed();
+        setContentView(R.layout.activity_contact_us);
     }
 }
