69c7496d730f064f15cf4366899c6c1d6717d81d
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 21:06:17 +1000

    creatd contact page

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/ContactUs.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/ContactUs.java
@@ -1,9 +1,12 @@
 package com.example.ships.myapplication.OtherInterfaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ships.myapplication.R;
+import com.example.ships.myapplication.homepageAndRegistration.MainActivity;
 
 public class ContactUs extends AppCompatActivity {
 
@@ -11,5 +14,9 @@ public class ContactUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+    }
+
+    public void goToHomePage(View view) {
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
