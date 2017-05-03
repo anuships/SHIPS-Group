6b0bfb5db06360d5e2f674a216c3d8f297389160
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 21:20:09 +1000

    created 'therapy factsheets' page

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/FAQs.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/FAQs.java
@@ -2,19 +2,14 @@ package com.example.ships.myapplication.OtherInterfaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.ships.myapplication.R;
 
-public class ThereapyFactsheets extends AppCompatActivity {
+public class FAQs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thereapy_factsheets);
-    }
-
-    public void goBack(View view) {
-        super.onBackPressed();
+        setContentView(R.layout.activity_faqs);
     }
 }
