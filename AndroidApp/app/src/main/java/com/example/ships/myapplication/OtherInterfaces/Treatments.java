ee32c96d1ab6fafcd5e352c7a4e841bf64652b53
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 23:18:28 +1000

    created Treatments page

---
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/Treatments.java
@@ -0,0 +1,22 @@
+package com.example.ships.myapplication.OtherInterfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ships.myapplication.R;
+
+public class Treatments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treatments);
+    }
+
+    public void goToSystematicDesensitisation(View view) {
+    }
+
+    public void goBack(View view) {
+    }
+}
