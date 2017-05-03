6677e0da470cd21e744ffb44961417fb2146cd9c
Yiuan Liang <u5855756@anu.edu.au>
2017-05-03 22:02:52 +1000

    refactor

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/FindPassword.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/OtherInterfaces/FindPassword.java
@@ -2,21 +2,14 @@ package com.example.ships.myapplication.OtherInterfaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.ships.myapplication.R;
 
-public class FindOutMore extends AppCompatActivity {
+public class FindPassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_find_out_more);
-    }
-
-    public void goToHomePage(View view) {
-    }
-
-    public void goToFAQs(View view) {
+        setContentView(R.layout.activity_find_password);
     }
 }
