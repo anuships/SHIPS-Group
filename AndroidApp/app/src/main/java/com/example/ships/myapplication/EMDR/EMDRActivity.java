0a2ca4faa6e5f4d4279a7b82102ddf2b05e33e18
Lalit Prasad <lalitpras@gmail.com>
2017-05-11 15:37:12 +1000

    attempts at adding sound to EMDR activity

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/EMDR/EMDRActivity.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/EMDR/EMDRActivity.java
@@ -1,9 +1,13 @@
 package com.example.ships.myapplication.EMDR;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.drawable.shapes.OvalShape;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +26,8 @@ import android.widget.TextView;
 
 import com.example.ships.myapplication.R;
 
+import java.io.IOException;
+
 import static com.example.ships.myapplication.R.layout.activity_emdr;
 import static com.example.ships.myapplication.R.layout.animationemdr;
 
@@ -35,8 +41,6 @@ public class EMDRActivity extends AppCompatActivity {
     private static final int EMDR_REPEATS = 10;
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,58 +48,36 @@ public class EMDRActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        final Button emdrButton = (Button) findViewById(R.id.start_emdr_button);
-//        emdrButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick (View v) {
-//                emdrButton.setVisibility(View.GONE);
+//                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+//        for (int i = 0; i < 10; i++) {
+//            audioManager.playSoundEffect(SoundEffectConstants.CLICK);
 //
-//
-//
-//                Display display = getWindowManager().getDefaultDisplay();
-//                Point size = new Point();
-//                display.getSize(size);
-//                int width = size.x;
-//                int height = size.y;
-//
-//                SCREEN_WIDTH = width;
-//                SCREEN_HEIGHT = height;
-//
-//                float emdrCircleDiameter = getResources().getDimension(R.dimen.emdr_circle_diameter);
-//
-//                AnimationSet ballMovementSet = new AnimationSet(true);
-//
-////                TranslateAnimation moveLeftToRight = new TranslateAnimation(-((SCREEN_WIDTH - emdrCircleDiameter)/2), ((SCREEN_WIDTH - emdrCircleDiameter)/2),
-////                        ((SCREEN_HEIGHT - emdrCircleDiameter)/2), ((SCREEN_HEIGHT - emdrCircleDiameter)/2));
-//                TranslateAnimation moveLeftToRight = new TranslateAnimation(-((SCREEN_WIDTH - emdrCircleDiameter)/2), ((SCREEN_WIDTH - emdrCircleDiameter)/2),
-//                        ((- emdrCircleDiameter)/2), (( - emdrCircleDiameter)/2));
-//                moveLeftToRight.setDuration(EMDR_DURATION);
-//                moveLeftToRight.setFillAfter(true);
-//                moveLeftToRight.setRepeatMode(Animation.REVERSE);
-//                moveLeftToRight.setRepeatCount(EMDR_REPEATS);
-//                ballMovementSet.addAnimation(moveLeftToRight);
-//
-//                TranslateAnimation moveUpAndDown = new TranslateAnimation(0, 0, 0, SCREEN_HEIGHT);
-//                moveUpAndDown.setDuration(3*EMDR_DURATION);
-//                moveUpAndDown.setFillAfter(true);
-//                moveUpAndDown.setRepeatMode(Animation.REVERSE);
-//                moveUpAndDown.setRepeatCount(EMDR_REPEATS);
-//                //ballMovementSet.addAnimation(moveUpAndDown);
-//
-//                View emdrView = findViewById(R.id.emdrlayout);
-//                emdrView.setVisibility(View.VISIBLE);
-//
-//
-//                LinearLayout layoutEMDRMovement = (LinearLayout) findViewById(R.id.emdrlayout);
-//                layoutEMDRMovement.startAnimation(ballMovementSet);
-////        for (int i = 0; i < 10; i++) {
-////            emdrView.playSoundEffect(SoundEffectConstants.CLICK);
-////        }
-//                layoutEMDRMovement.playSoundEffect(SoundEffectConstants.CLICK);
-////        emdrView.playSoundEffect(SoundEffectConstants.CLICK);
-//
-//            }
-//        });
+//        }
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.ticksound);
 
+//        final Handler handler = new Handler();
+//        final int delay = EMDR_DURATION; //milliseconds
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                mediaPlayer.start();
+//                mediaPlayer.release();
+//                handler.postDelayed(this, delay);
+//            }
+//        }, delay);
+
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.ticksound);
+//        for (int i = 0; i < EMDR_REPEATS; i++) {
+//            mediaPlayer.start();
+//            try {
+//                mediaPlayer.prepare();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+
+//        mediaPlayer.setLooping(true);
+//        mediaPlayer.start();
 
                 Display display = getWindowManager().getDefaultDisplay();
                 Point size = new Point();
@@ -136,8 +118,12 @@ public class EMDRActivity extends AppCompatActivity {
 //        for (int i = 0; i < 10; i++) {
 //            emdrView.playSoundEffect(SoundEffectConstants.CLICK);
 //        }
-                layoutEMDRMovement.playSoundEffect(SoundEffectConstants.CLICK);
-//        emdrView.playSoundEffect(SoundEffectConstants.CLICK);
+//                layoutEMDRMovement.playSoundEffect(SoundEffectConstants.CLICK);
+////        emdrView.playSoundEffect(SoundEffectConstants.CLICK);
+//
+//        View viewEMDRMovement = findViewById(R.id.emdr_circle_layout);
+//        viewEMDRMovement.playSoundEffect(SoundEffectConstants.CLICK);
+
 
 
         Button start_emdr_settings_button = (Button) findViewById(R.id.start_emdr_settings_button);
@@ -146,8 +132,6 @@ public class EMDRActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
 
             }
 
