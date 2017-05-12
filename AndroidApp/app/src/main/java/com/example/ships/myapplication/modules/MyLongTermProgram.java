984274c12582522afe9adc4087c30f5dc906de58
Yiuan Liang <u5855756@anu.edu.au>
2017-05-13 03:00:42 +1000

    added long/term my program

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/modules/MyLongTermProgram.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/modules/MyLongTermProgram.java
@@ -6,18 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.ships.myapplication.FAS.FAS;
+import com.example.ships.myapplication.GSR.GSRGraphActivity;
 import com.example.ships.myapplication.OtherInterfaces.Records;
+import com.example.ships.myapplication.OtherInterfaces.SystematicDesensitation;
 import com.example.ships.myapplication.OtherInterfaces.ThereapyFactsheets;
 import com.example.ships.myapplication.OtherInterfaces.Treatments;
 import com.example.ships.myapplication.OtherInterfaces.UserProfile;
 import com.example.ships.myapplication.R;
+import com.example.ships.myapplication.relaxationAudio.RelaxationAudioActivity;
 
 public class MyLongTermProgram extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_program);
+        setContentView(R.layout.activity_my_long_term_program);
     }
 
     public void goToFactSheet(View view) {
@@ -45,4 +48,15 @@ public class MyLongTermProgram extends AppCompatActivity {
     }
 
 
+    public void goToRelaxationTraning(View view) {
+        startActivity(new Intent(this, RelaxationAudioActivity.class));
+    }
+
+    public void goToBiofeedback(View view) {
+        startActivity(new Intent(this, GSRGraphActivity.class));
+    }
+
+    public void goToSystematicDesensitisation(View view) {
+        startActivity(new Intent(this, SystematicDesensitation.class));
+    }
 }
--- AndroidApp/app/src/main/java/com/example/ships/myapplication/modules/MyShortTermProgram.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/modules/MyShortTermProgram.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.example.ships.myapplication.EMDR.EMDRActivity;
 import com.example.ships.myapplication.FAS.FAS;
 import com.example.ships.myapplication.OtherInterfaces.Records;
 import com.example.ships.myapplication.OtherInterfaces.ThereapyFactsheets;
@@ -17,7 +18,7 @@ public class MyShortTermProgram extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_program);
+        setContentView(R.layout.activity_my_short_term_program);
     }
 
     public void goToFactSheet(View view) {
@@ -28,13 +29,6 @@ public class MyShortTermProgram extends AppCompatActivity {
         startActivity(new Intent(this, FAS.class));
     }
 
-    public void goToTherapeuticTools(View view) {
-        startActivity(new Intent(this, Programs.TherapeuticTools.class));
-    }
-
-    public void goToTreatment(View view) {
-        startActivity(new Intent(this, Treatments.class));
-    }
 
     public void goBack(View view) {
         startActivity(new Intent(this, UserProfile.class));
@@ -45,4 +39,7 @@ public class MyShortTermProgram extends AppCompatActivity {
     }
 
 
+    public void goToEMDR(View view) {
+        startActivity(new Intent(this, EMDRActivity.class));
+    }
 }
