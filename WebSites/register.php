c159c0f14108af6b0fa09f728580ff45824aa3ac
Yiuan Liang <u5855756@anu.edu.au>
2017-07-21 23:24:21 +1000

    initial register page

---
+++ WebSites/register.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="register.css">
+<title>SHIPS-ANU Register</title>
+</head>
+
+<body>
+<div id="content">
+    <h1 align="center">Registration</h1>
+    <div id="register_form">
+    <table border="0">
+      <tr>
+        <td align="right"><b>Email:</b></td>
+        <td><input type="email" name="email" id="req" size="20" maxlength="30" /></td>
+      </tr>  
+      <tr>
+        <td align="right"><b> password:</b></td>
+        <td><input type="password" name="password" id="req" size="20" maxlength="30"></td>
+      </tr>
+      <tr>
+        <td align="right"><b> Confirm Password:</b></td>
+        <td><input type="password" name="confirm_password" id="req" size="20" maxlength="30"></td>
+      </tr>
+      <tr>
+        <td align="right"><b> First Name:</b></td>
+        <td><input type="text" name="name" size="20" id="req" maxlength="30"></td>
+      </tr>
+      <tr>
+        <td align="right"><b> Last Name:</b></td>
+        <td><input type="text" name="name" size="20" id="req" maxlength="30"></td>
+      </tr>
+      <tr>
+        <td align="right"><b> Preferred User Name:</b></td>
+        <td><input type="text" name="name" size="20" maxlength="30"></td>
+      </tr>
+      </table>
+    <button>submit</button>
+    </div>
+</div>
+</body>
+</html>
