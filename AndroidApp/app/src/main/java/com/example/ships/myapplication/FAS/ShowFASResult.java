47e134d271a73941f053e342f029773bfaeeeb78
Ka Wai Ma <u5968223@anu.edu.au>
2017-08-04 23:15:44 +1000

    FAS score interpretation and recommendation added and correct errors in calculating scores

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/FAS/ShowFASResult.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/FAS/ShowFASResult.java
@@ -6,21 +6,93 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.ships.myapplication.OtherInterfaces.UserProfile;
 import com.example.ships.myapplication.modules.AllPrograms;
 import com.example.ships.myapplication.R;
+import com.example.ships.myapplication.modules.MyLongTermProgram;
 
 public class ShowFASResult extends AppCompatActivity {
     TextView tv;
+    TextView tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_fasresult);
         tv = (TextView) findViewById(R.id.textView);
-        tv.setText(Integer.toString(FAS.getScore()));
+        tv2 = (TextView) findViewById(R.id.textView37);
+        String scoreComment;
+        String AFscoreComment;
+        String IFscoreComment;
+        String GFscoreComment;
+
+        if (FAS.getScore() >= 82.74){
+            scoreComment = "You are very likely suffered from fear of flying";
+        } else if (FAS.getScore()>=62){
+            scoreComment = "You are likely suffered from fear of flying";
+        } else {
+            scoreComment = "You are unlikely suffered from fear of flying";
+        }
+
+        if (FAS.getAFscore() >= 33){
+            AFscoreComment = "You are very likely to have anticipatory flight anxiety";
+        } else if (FAS.getAFscore()>=22){
+            AFscoreComment = "You are likely to have anticipatory flight anxiety";
+        } else {
+            AFscoreComment = "You are unlikely to have anticipatory flight anxiety";
+        }
+
+        if (FAS.getIFscore() >= 34){
+            IFscoreComment = "You are very likely to have in-flight anxiety";
+        } else if (FAS.getIFscore()>=21){
+            IFscoreComment = "You are likely to have in-flight anxiety";
+        } else {
+            IFscoreComment = "You are unlikely to have in-flight anxiety";
+        }
+
+        if (FAS.getGFscore() >= 13){
+            GFscoreComment = "You are very likely to have generalized flight anxiety";
+        } else if (FAS.getGFscore()>=7){
+            GFscoreComment = "You are likely to have generalized flight anxiety";
+        } else {
+            GFscoreComment = "You are unlikely to have generalized flight anxiety";
+        }
+
+        tv.setText(
+                "1. Your total score of fear of flying is " + Integer.toString(FAS.getScore()) + ": " + "\n" + scoreComment + "\n" + "\n" +
+                "2. Your total score of anticipatory flight anxiety is " + Integer.toString(FAS.getAFscore()) + ": " + "\n" + AFscoreComment + "\n" + "\n" +
+                "3. Your total score of in-flight anxiety is " + Integer.toString(FAS.getIFscore()) + ": " + "\n" + IFscoreComment + "\n" + "\n" +
+                "4. Your total score of generalized flight anxiety is " + Integer.toString(FAS.getGFscore()) + ": " + "\n"  + GFscoreComment + "\n"
+        );
+
+        String suggestion;
+        if (FAS.getScore()>=82.74){
+            suggestion = "Based on your score, we recommend you to choose the following modules: " + "\n" +
+                    "1. Systematic Desensitization" + "\n" +
+                    "2. EMDR" + "\n" +
+                    "3. Cognitive Therapy" + "\n" +
+                    "4. Biofeedback" + "\n" +
+                    "5. Relaxation Audio Training" + "\n";
+        } else if (FAS.getScore() >=67){
+            suggestion = "Based on your score, we recommend you to choose the following modules: " + "\n" +
+                    "1. EMDR" + "\n" +
+                    "2. Cognitive Therapy" + "\n" +
+                    "3. Biofeedback" + "\n" +
+                    "4. Relaxation Audio Training" + "\n";
+        } else{
+            suggestion = "Although you are unlikely to have fear of flying, the following modules can still be helpful to you: " + "\n" +
+                    "1. Biofeedback" + "\n" +
+                    "2. Relaxation Audio Training" + "\n";
+        }
+
+        tv2.setText(suggestion);
     }
 
     public void goBack(View v)
     {
-        startActivity(new Intent(this, AllPrograms.class));
+        startActivity(new Intent(this, FAS.class));
+    }
+    public void goToTreatment(View v)
+    {
+        startActivity(new Intent(this, MyLongTermProgram.class));
     }
 }
