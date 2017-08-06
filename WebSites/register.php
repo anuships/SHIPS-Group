b2307656b90106022e968cebff8e2fc22703312d
Yiuan Liang <u5855756@anu.edu.au>
2017-08-07 02:50:07 +1000

    tried to implement register with database but failed

--- WebSites/register.php
+++ WebSites/register.php
@@ -1,18 +1,26 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="css/general.css">
 <link rel="stylesheet" type="text/css" href="css/register.css">
+<script type="text/JavaScript" src="jquery-1.11.0.min.js"></script> 
 <title>SHIPS-ANU Register</title>
 </head>
 
 <body>
 <div id="header"></div>
 <div id="content">
+<form id="register" method="POST" action="registercheck.php">
     <h1 align="center">Registration</h1>
+   <?php 
+   	$test = "1";
+	echo "<p>".$test. "</p>";
    
-    <table border="0">
+   ?>    
+   
+   <table border="0">
       <tr>
         <td align="right"><b>Email:</b></td>
         <td><input type="email" name="email" id="req" size="20" maxlength="30" /></td>
@@ -25,7 +33,8 @@
       </tr>
       <tr>
         <td align="right"><b> Confirm Password:</b></td>
-        <td><input type="password" name="confirm_password" id="req" size="20" maxlength="30"></td>
+        <td><input type="password" name="confirm_password" 
+        			id="req" size="20" maxlength="30"></td>
         <td id="req">*</td>
       </tr>
       <tr>
@@ -47,12 +56,12 @@
       <p align="right" id="hints">* are required fileds </p>
        <table id="buttons">
          <tr>
-            <td><button>Back</button></td>
-            <td><button>Finish</button></td>
+            <td><button >Back</button></td>
+            <td><button type="submit">Finish</button></td>
          </tr>
     	</table>
       
-    
+</form>    
 </div>
 <div id="footer"></div>
 </body>
---
+++ WebSites/registercheck.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/general.css">
+<link rel="stylesheet" type="text/css" href="css/register.css">
+<title>SHIPS-ANU Register Checking</title>
+
+<script>
+function goToMain() {
+  window.location.assign("./ships.html");
+}
+</script>
+
+</head>
+
+<body>
+<div id="header"></div>
+<div id="content">
+	<div style="width:100px; margin:auto;">
+    	<p style="font-size:20px">Sucessful</p>
+    </div>
+    <div style="width:200px; margin:auto;">
+	    <button onclick="goToMain">Go back to Home Page</button>
+    </div>
+    
+    <?php		
+		$host="localhost"; // Host name 
+		$a_name="ships"; // Mysql username 
+		$pw="123"; // Mysql password 
+		$db_name="anu-ships"; // Database name 
+		
+		
+		// Connect to server and select database.
+		$link=mysql_connect("$host", "$a_name", "$pw")or die("cannot connect"); 
+		mysql_select_db("$db_name")or die("cannot select database");		
+		
+		// username and password sent from form 
+		$email = $_POST["email"];
+		$firstName=$_POST["fname"]; 
+		$lastName=$_POST["lname"]; 
+		$password = $_POST["password"];
+		$preferName=$_POST["name"]; 
+		
+		
+		$sql="SELECT * FROM user WHERE email='" . $email . "'";
+		
+		echo ("<p>testing 1</p>");
+		//should stop
+		echo "<p>" .$sql . "</p>";
+		
+		$result=mysql_query($sql);
+		$count=mysql_num_rows($result);
+		
+		$sql="INSERT INTO `anu-ships`.`user` ( `email` , `firstName` , `lastName` , `password` , `preferName`)
+		VALUES('" . $email . "','" . $firstName . "','" . $lastName . "','" . $password . "','" . $preferName . "')";
+		$result=mysql_query($sql);
+	
+		if ($link->query($sql) === TRUE) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $sql . "<br>" . $link->error;
+		}
+		
+		mysql_close($link); 
+		?>
+
+</div>
+
+<div id="footer"></div>
+</body>
+</html>
+
