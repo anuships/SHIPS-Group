db9207b4e5d0cbaed2aa30452124a180082f5b31
Lalit Prasad <lalitpras@gmail.com>
2017-08-30 16:09:57 +1000

    made changes to emdr, still trying to debug

--- WebSites/wp-content/themes/agama/js/emdr.js
+++ WebSites/wp-content/themes/agama/js/emdr.js
@@ -1,12 +1,12 @@
 var canvas = document.getElementById('canvas');
 alert('sees changes');
 
-function init() {
-   alert('reached init');
-   window.requestAnimationFrame(draw);
-}
+//function init() {
+//   alert('reached init');
+//   window.requestAnimationFrame(draw);
+//}
 
-function draw() {
+//function draw() {
    var ctx = canvas.getContext('2d');
    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
    ctx.fillStyle = "white";
@@ -16,8 +16,8 @@ function draw() {
 //   ctx.stroke();
    
 //   window.requestAnimationFrame(draw);
-}
+//}
 
-init();
+//init();
 
 
