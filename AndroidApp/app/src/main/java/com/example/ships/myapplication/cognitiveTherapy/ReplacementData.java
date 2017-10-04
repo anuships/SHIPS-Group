0ea98fadcd50e75e494789ad217cda1603d9322f
Lalit Prasad <lalitpras@gmail.com>
2017-10-04 11:04:47 +1100

    fixed typos in ReplacementData

--- AndroidApp/app/src/main/java/com/example/ships/myapplication/cognitiveTherapy/ReplacementData.java
+++ AndroidApp/app/src/main/java/com/example/ships/myapplication/cognitiveTherapy/ReplacementData.java
@@ -8,7 +8,7 @@ class ReplacementData {
     public static void initializeReplacement(ReplacementHandler db) {
 
         db.addContent(new ReplacementContent(1, "SHOULDS\nChange from demands to preferences","Preferences:\nI \'d like to\nI \'d prefer\nI \'d rather\nIt would be better if\nI want to\nI choose to", "Demands:\nShould\nMust\nHave to\nOught to\nGot to\nSupposed to\nNeed"));
-        db.addContent(new ReplacementContent(2, "AWFULISING\nChange from catastrophic to realistic","Realistic:\nIt\'s uncomfortable\nIt\'s unpleasant\nIt\'s a nuisance\nIt\'s undesirable\nI don\'t like it\nIt\'s embarrassing\n", "Catestrophic:\nTerrible\nAwful\nHorrible\nDigusting\nCan\'t stand it\nUnbearabble"));
+        db.addContent(new ReplacementContent(2, "AWFULISING\nChange from catastrophic to realistic","Realistic:\nIt\'s uncomfortable\nIt\'s unpleasant\nIt\'s a nuisance\nIt\'s undesirable\nI don\'t like it\nIt\'s embarrassing\n", "Catastrophic:\nTerrible\nAwful\nHorrible\nDigusting\nCan\'t stand it\nUnbearable"));
         db.addContent(new ReplacementContent(3, "ABSOLUTE\nChange from absolutes to relative","Relative:\nSometimes","Absolutes:\nAlways\nNever"));
         db.addContent(new ReplacementContent(4, "SELF-LIMITING\nChange from self-limiting to self-enhancing","Self-enhancing:\nCan\nWon\'t\nHaven\'t up till now","Self-limiting:\nCan\'t"));
         db.addContent(new ReplacementContent(5, "BEING (TOTAL SELF)\nChange from being (total self) to doing/behaviour","Doing/Behaviour:\nI do","Being (Total self):\nI am"));
